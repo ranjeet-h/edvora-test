2d087f305f1ff4da6f5b015a7a874eb6838cd4d9
Ranjeet <ranjeet.h@stackera.io>
2021-12-14 18:03:12 +0530

    working treee

--- components/DetailsScreen.tsx
+++ components/DetailsScreen.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import RightArrow from '../assets/right-arrow.svg';
 import TestImage from '../assets/Rectangle.png';
 import { useGetData } from '../helpers/getQuery';
+import Slider from 'react-slick';
 
 interface Props {
   product: string;
@@ -21,8 +22,8 @@ const DetailsScreen = ({ state, city, product }: Props) => {
     if (state && city && product) {
       const filteredArr = data.filter(
         (item: any) =>
-          item.address.state === state.toString() &&
-          item.address.city === city.toString() &&
+          item.address.state === state.toString() ||
+          item.address.city === city.toString() ||
           item.address.state === state.toString()
       );
       setFilteredArr(filteredArr);
@@ -37,16 +38,47 @@ const DetailsScreen = ({ state, city, product }: Props) => {
     }
   }, [city, data, isFetching, isLoading, product, state]);
 
-  if (!isLoading && !isFetching && data) {
-    console.log(
-      'test',
-      data.filter(
-        (item: any, index: any, array: any) =>
-          item.product_name === 'Saudi Aramco'
-      )
+  // if (!isLoading && !isFetching && data) {
+  //   console.log(
+  //     'test',
+  //     data.filter(
+  //       (item: any, index: any, array: any) =>
+  //         item.product_name === 'Saudi Aramco'
+  //     )
+  //   );
+  // }
+  // console.log('filteredArr', filteredArr);
+
+  const settings = {
+    slidesToShow: 1,
+    swipeToSlide: true,
+    adaptiveHeight: true,
+    slidesToScroll: 3,
+    infinite: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
+
+  function SampleNextArrow(props: any) {
+    const { className, style, onClick } = props;
+    return (
+      <div onClick={onClick} className="cursor-pointer ">
+        <Image src={RightArrow} alt="" width="24" height="24" />
+      </div>
     );
   }
-  console.log('filteredArr', filteredArr);
+
+  function SamplePrevArrow(props: any) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        onClick={onClick}
+        className="z-40 transform rotate-180 cursor-pointer "
+      >
+        <Image src={RightArrow} alt="" width="24" height="24" />
+      </div>
+    );
+  }
   return (
     <div>
       <h1 className="text-3xl font-bold text-white">Edvora</h1>
@@ -64,28 +96,28 @@ const DetailsScreen = ({ state, city, product }: Props) => {
               'Products'
             )}
           </h6>
-          <hr className="w-10/12 h-[2px] bg-[#CBCBCB80] my-2" />
-          <div className="flex gap-3 ">
-            <div className="flex items-center gap-2 p-4 rounded-xl bg-[#131313] w-10/12 h-48  overflow-hidden  ">
+          <hr className="w-10/12  bg-[#CBCBCB80] my-2" />
+          <div className="flex w-full h-full gap-3 ">
+            <div className="flex items-center gap-2 p-4 rounded-xl bg-[#131313] w-10/12 h-48  overflow-x-scroll __hide-scrollbar overflow-hidden   ">
               {!isLoading &&
                 !isFetching &&
                 data &&
                 filteredArr &&
                 filteredArr
-                  .slice(0, 4)
+                  .slice(0, 14)
                   .map((item: any, index: any) => (
                     <div
                       key={index}
-                      className="w-[310px] h-full my-2 p-3 bg-[#232323] rounded-lg flex flex-col gap-2 hover:scale-110 transition-all cursor-pointer "
+                      className=" h-[150px] w-[230px] my-2 p-3 bg-[#232323] rounded-lg flex flex-col  hover:scale-110 transition-all cursor-pointer  "
                     >
-                      <div className="flex items-center gap-6">
+                      <div className="flex items-center w-full gap-6">
                         <img
                           src={item.image ? item.image : TestImage}
                           alt=""
                           className="w-16 h-16 rounded-lg"
                         />
-                        <div className="flex flex-col gap-2">
-                          <h3 className="text-sm font-normal text-white truncate">
+                        <div className="flex flex-col w-full gap-2">
+                          <h3 className="w-9/12 text-sm font-normal text-white truncate">
                             {item.product_name}
                           </h3>
                           <h4 className="text-[#ffffff94] text-xs font-normal">
@@ -96,16 +128,16 @@ const DetailsScreen = ({ state, city, product }: Props) => {
                           </h6>
                         </div>
                       </div>
-                      <div className="flex gap-5">
-                        <h6 className="text-[#ffffff94] text-xs font-normal">
+                      <div className="flex gap-5 mt-2">
+                        <h6 className="text-[#ffffff94] text-xs font-normal truncate w-1/2">
                           {item.address.state} {item.address.city}
                         </h6>
                         <h6 className="text-[#ffffff94] text-xs font-normal">
-                          Date:{' '}
+                          Date:
                           {new Date(item.date).toLocaleDateString()}
                         </h6>
                       </div>
-                      <h4 className="text-[#ffffff94] text-xs font-normal">
+                      <h4 className="text-[#ffffff94] text-xs font-normal mt-1">
                         {item.discription}
                       </h4>
                     </div>
@@ -129,50 +161,63 @@ const DetailsScreen = ({ state, city, product }: Props) => {
 
             return (
               <>
-                <h6 className="mt-20 text-xl font-normal text-white">
+                <h6 className="text-xl font-normal text-white mt-14">
                   {product_name}
                 </h6>
                 <hr className="w-10/12 h-[2px] bg-[#CBCBCB80] my-2 " />
-                <div className="flex w-full gap-3">
-                  <div className="flex items-center gap-2 p-4 rounded-xl bg-[#131313] w-10/12 h-48  overflow-hidden  ">
-                    {products
-                      .slice(0, 4)
-                      .map(({ brand_name }: any) => (
-                        <>
-                          <div className="w-[310px] h-full my-2 p-3 bg-[#232323] rounded-lg flex flex-col gap-2 hover:scale-110 transition-all cursor-pointer ">
-                            <div className="flex items-center gap-6">
-                              <Image
-                                src={TestImage}
-                                alt=""
-                                width="70"
-                                height="70"
-                              />
-                              <div className="flex flex-col gap-2">
-                                <h3 className="text-sm font-normal text-white">
-                                  {product_name}
-                                </h3>
-                                <h4 className="text-[#ffffff94] text-xs font-normal">
-                                  {brand_name}
-                                </h4>
-                                <h6 className="text-sm text-white">
-                                  $ 29.99
-                                </h6>
-                              </div>
-                            </div>
-                            <div className="flex gap-5">
-                              <h6 className="text-[#ffffff94] text-xs font-normal">
-                                Location
+                <div className="flex w-full gap-3 mb-10">
+                  <div className="flex items-center gap-2 p-4 rounded-xl bg-[#131313] w-10/12 h-48  overflow-x-scroll __hide-scrollbar overflow-hidden   ">
+                    {products.map(
+                      (
+                        {
+                          brand_name,
+                          price,
+                          address: { state, city },
+                          date,
+                          discription,
+                          image,
+                        }: any,
+                        index: any
+                      ) => (
+                        // <Slider {...settings} key={index}>
+                        <div
+                          key={index}
+                          className=" h-[150px] w-[230px] my-2 p-3 bg-[#232323] rounded-lg flex flex-col  hover:scale-110 transition-all cursor-pointer  "
+                        >
+                          <div className="flex items-center w-full gap-6">
+                            <img
+                              src={image ? image : TestImage}
+                              alt=""
+                              className="w-16 h-16 rounded-lg"
+                            />
+                            <div className="flex flex-col w-full gap-2">
+                              <h3 className="w-9/12 text-sm font-normal text-white truncate">
+                                {product_name}
+                              </h3>
+                              <h4 className="text-[#ffffff94] text-xs font-normal">
+                                {brand_name}
+                              </h4>
+                              <h6 className="text-sm text-white">
+                                $ {price}
                               </h6>
-                              <h6 className="text-[#ffffff94] text-xs font-normal">
-                                Date:10:12:2021
-                              </h6>
                             </div>
-                            <h4 className="text-[#ffffff94] text-xs font-normal">
-                              Description of the Product / Item
-                            </h4>
                           </div>
-                        </>
-                      ))}
+                          <div className="flex gap-5 mt-2">
+                            <h6 className="text-[#ffffff94] text-xs font-normal truncate w-1/2">
+                              {state} {city}
+                            </h6>
+                            <h6 className="text-[#ffffff94] text-xs font-normal">
+                              Date:
+                              {new Date(date).toLocaleDateString()}
+                            </h6>
+                          </div>
+                          <h4 className="text-[#ffffff94] text-xs font-normal mt-1">
+                            {discription}
+                          </h4>
+                        </div>
+                        // </Slider>
+                      )
+                    )}
                   </div>
                   <div className="flex items-center justify-center w-1/12 cursor-pointer">
                     <Image
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,32 @@ import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import 'slick-carousel/slick/slick.css';
+
+import 'slick-carousel/slick/slick-theme.css';
+import Head from 'next/head';
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <div className="">
+        <Head>
+          <link
+            rel="stylesheet"
+            type="text/css"
+            charSet="UTF-8"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </div>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
